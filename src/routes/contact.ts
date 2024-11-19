327fbd33702f3e3db703fbb3a194f9d9887a81bd
Edgar Sousa <stratulatedgar@gmail.com>
2024-11-19 16:03:47 +0000

    feat(routes): add GET and DELETE methods for contact route
    Enable viewing and deleting contacts using the new methods.

--- src/routes/contact.ts
+++ src/routes/contact.ts
@@ -1,8 +1,12 @@
+import { error } from "console";
 import express from "express";
+import { readFile, writeFile } from "fs/promises";
+
+const dataSource = "./data/list.txt";
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { name, phone } = req.body;
 
   const validateName = (name: string): boolean => {
@@ -33,7 +37,55 @@ router.post("/", (req, res) => {
     res.status(400).json({ error: "Phone Number is invalid!" });
   }
 
+  //Processamento de dados
+  let list: string[] = [];
+  try {
+    const data = await readFile(dataSource, { encoding: "utf8" });
+
+    list = data.split("\n");
+  } catch (err) {}
+
+  list.push(name, phone);
+  await writeFile(dataSource, list.join("\n"));
+
   res.status(201).json({ contact: name, phone: phone });
+});
+
+router.get("/", async (req, res) => {
+  let list: string[] = [];
+  try {
+    const data = await readFile(dataSource, { encoding: "utf8" });
+
+    list = data.split("\n");
+  } catch (err) {}
+
+  res.status(200).json({ contact: list });
+});
+
+router.delete("/", async (req, res) => {
+  const { name, phone } = req.query;
+
+  if (!name || !phone) {
+    res
+      .status(400)
+      .json({ error: "Precisa de  um nome e um número para eliminar" });
+    return;
+  }
+
+  let list: string[] = [];
+  try {
+    const data = await readFile(dataSource, { encoding: "utf8" });
+
+    list = data.split("\n");
+  } catch (err) {}
+
+  list = list.filter((item) => {
+    return item !== name && item !== phone;
+  });
+
+  await writeFile(dataSource, list.join("\n"));
+
+  res.status(200).json({ contact: name, phone: phone });
 });
 
 export default router;
